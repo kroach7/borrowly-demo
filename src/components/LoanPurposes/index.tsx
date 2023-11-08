f6f2753ba8add507b1286bed29d734641024868e
KRoach <kyle_roach7@outlook.com>
2023-11-07 23:12:15 -0500

    add better rendering on categories

--- src/components/LoanPurposes/index.tsx
+++ src/components/LoanPurposes/index.tsx
@@ -1,14 +1,39 @@
-import { getLoanPurposes } from "@src/services/api";
-import { LoanPurposesItem } from "@src/components/";
+"use client";
 
-export default async function LoanPurposes() {
-  const data = await getLoanPurposes();
+import React, { useState, useEffect } from "react";
+import { getLoanPurposes } from "@src/services/api";
+import LoanPurposesItem from "@src/components/LoanPurposesItem"; // Correct import path
+import { useSearchParams } from "next/navigation";
+
+export default function LoanPurposes() {
+
+  const searchParams = useSearchParams();
+  const purpose_name = searchParams.get("purpose_name") || "";
+  const [selectedPurpose, setSelectedPurpose] = useState(purpose_name);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const loanPurposes = await getLoanPurposes();
+      setData(loanPurposes);
+    }
+    fetchData();
+  }, []);
+
+  const handlePurposeChange = (purposeName: any) => {
+    setSelectedPurpose(purposeName);
+  };
 
   return (
     <div className="grid grid-cols-[1fr_1fr_1fr_1fr_1fr] gap-[17px] lg:grid-cols-[1fr_1fr_1fr_1fr] md:grid-cols-[1fr_1fr_1fr] sm:grid-cols-[1fr_1fr]">
       {data &&
         data.map((item: any, index: number) => (
-          <LoanPurposesItem key={index} item={item} />
+          <LoanPurposesItem
+            key={index}
+            item={item}
+            isSelected={selectedPurpose === item.purpose_name}
+            onPurposeClick={handlePurposeChange}
+          />
         ))}
     </div>
   );
--- src/components/LoanPurposesItem/index.tsx
+++ src/components/LoanPurposesItem/index.tsx
@@ -1,40 +1,36 @@
 "use client";
 
+import React, { useState } from "react";
 import classNames from "classnames";
 import { useRouter } from "next/navigation";
 import { useSearchParams } from "next/navigation";
 
-const LoanPurposesItem = ({ item }: { item: any }) => {
+const LoanPurposesItem = ({ item, isSelected, onPurposeClick }: { item: any, isSelected: any, onPurposeClick: any }) => {
   const router = useRouter();
-
   const searchParams = useSearchParams();
-  const purpose_name = searchParams.get("purpose_name") || "";
   const params = new URLSearchParams(searchParams);
+
+  const handleItemClick = () => {
+    const newPurposeName = isSelected ? "" : item.purpose_name;
+    onPurposeClick(newPurposeName);
+    params.set("purpose_name", newPurposeName);
+    router.push(`/?${params.toString()}`, {
+      scroll: false,
+    });
+  };
 
   return (
     <div
       key={item.purpose_id}
       className={classNames(
         "rounded-xl bg-white p-3 cursor-pointer hover:bg-[#2BB8B8] hover:text-white duration-200",
-        item.purpose_name === purpose_name && "!bg-[#2BB8B8] text-white"
+        isSelected && "!bg-[#2BB8B8] text-white"
       )}
-      onClick={() => {
-        if(purpose_name == item.purpose_name) {
-          params.set("purpose_name", ``);
-          router.push(`/?${params.toString()}`, {
-            scroll: false,
-          });
-        } else {
-          params.set("purpose_name", `${item.purpose_name}`);
-          router.push(`/?${params.toString()}`, {
-            scroll: false,
-          });
-        }
-      }}
+      onClick={handleItemClick}
     >
       <div className="h-[72px] w-full my-[5px] rounded-xl bg-[#F3F3F3] flex justify-start overflow-hidden">
-        <img src={item.purpose_url} className=" h-full object-cover py-[12px] px-[16px]" />
-      </div>      
+        <img src={item.purpose_url} className="h-full object-cover object-center" style={{ padding: '12px 16px' }} />
+      </div>
       <span className="text-[12px] font-semibold leading-[166%]">
         {item.purpose_name}
       </span>
